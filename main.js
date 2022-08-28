a08f5cfb49f649a7d70ef7023751e8819e074ec6
AustinLeath <austinleath18@gmail.com>
2022-08-27 23:39:22 -0500

    Create main.js

---
+++ main.js
@@ -0,0 +1,22 @@
+const http = require('http');
+const { exec } = require('child_process');
+const requestListener = function (req, res) {
+    res.writeHead(200);
+
+exec('sensors|grep "high"|grep "Core"|cut -d "+" -f2|cut -d "." -f1|sort -nr|sed -n 1p', (error, stdout, stderr) => {
+    if (error) {
+        console.error(`error: ${error.message}`);
+        return;
+    }
+
+    if (stderr) {
+        console.error(`stderr: ${stderr}`);
+        return;
+    }
+
+    res.end(`r710_cpu_temp ${stdout}`);
+    });
+}
+
+const server = http.createServer(requestListener);
+server.listen(8080);
