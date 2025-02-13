d096d5dd745486c3f2bfbe449b05aad48ddca8e4
Austin Leath <austinleath18@gmail.com>
2025-02-13 02:46:37 -0600

    add content type header to achieve prometheus scraper compliance

--- main.js
+++ main.js
@@ -1,22 +1,25 @@
-const http = require('http');
-const { exec } = require('child_process');
+const http = require("http");
+const { exec } = require("child_process");
 const requestListener = function (req, res) {
-    res.writeHead(200);
+  res.writeHead(200, { "Content-Type": "text/plain" });
 
-exec('sensors|grep "high"|grep "Core"|cut -d "+" -f2|cut -d "." -f1|sort -nr|sed -n 1p', (error, stdout, stderr) => {
-    if (error) {
+  exec(
+    'sensors|grep "high"|grep "Core"|cut -d "+" -f2|cut -d "." -f1|sort -nr|sed -n 1p',
+    (error, stdout, stderr) => {
+      if (error) {
         console.error(`error: ${error.message}`);
         return;
-    }
+      }
 
-    if (stderr) {
+      if (stderr) {
         console.error(`stderr: ${stderr}`);
         return;
-    }
+      }
 
-    res.end(`server_cpu_temp ${stdout}`);
-    });
-}
+      res.end(`server_cpu_temp ${stdout}`);
+    }
+  );
+};
 
 const server = http.createServer(requestListener);
 server.listen(8080);
